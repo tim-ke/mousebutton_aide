41c845ae29703fe5b5ba9d8096eb2a43bc25bcb5
柯廷宗 <bravo230352@gmail.com>
2023-10-25 04:18:32 +0800

    v1.0

---
+++ WindowsFormsApp32/Form1.Designer.cs
@@ -0,0 +1,198 @@
+﻿
+namespace WindowsFormsApp32
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 設計工具所需的變數。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清除任何使用中的資源。
+        /// </summary>
+        /// <param name="disposing">如果應該處置受控資源則為 true，否則為 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 設計工具產生的程式碼
+
+        /// <summary>
+        /// 此為設計工具支援所需的方法 - 請勿使用程式碼編輯器修改
+        /// 這個方法的內容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.radioButton_left = new System.Windows.Forms.RadioButton();
+            this.radioButton_right = new System.Windows.Forms.RadioButton();
+            this.panel1 = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(72, 269);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(108, 40);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "開始";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(281, 269);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(108, 40);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "停止";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label1.Location = new System.Drawing.Point(68, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(112, 24);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "觸發時間:";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("新細明體", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.numericUpDown1.Increment = new decimal(new int[] {
+            10,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Location = new System.Drawing.Point(186, 38);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            99999,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            5,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(124, 35);
+            this.numericUpDown1.TabIndex = 3;
+            this.numericUpDown1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.numericUpDown1.Value = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label5.Location = new System.Drawing.Point(339, 34);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(118, 30);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "未啟動~";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("新細明體", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.label6.Location = new System.Drawing.Point(394, 88);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(27, 30);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "0";
+            this.label6.Click += new System.EventHandler(this.label6_Click);
+            // 
+            // radioButton_left
+            // 
+            this.radioButton_left.AutoSize = true;
+            this.radioButton_left.Checked = true;
+            this.radioButton_left.Location = new System.Drawing.Point(21, 23);
+            this.radioButton_left.Name = "radioButton_left";
+            this.radioButton_left.Size = new System.Drawing.Size(88, 19);
+            this.radioButton_left.TabIndex = 11;
+            this.radioButton_left.TabStop = true;
+            this.radioButton_left.Text = "滑鼠左鍵";
+            this.radioButton_left.UseVisualStyleBackColor = true;
+            // 
+            // radioButton_right
+            // 
+            this.radioButton_right.AutoSize = true;
+            this.radioButton_right.Location = new System.Drawing.Point(135, 23);
+            this.radioButton_right.Name = "radioButton_right";
+            this.radioButton_right.Size = new System.Drawing.Size(88, 19);
+            this.radioButton_right.TabIndex = 12;
+            this.radioButton_right.Text = "滑鼠右鍵";
+            this.radioButton_right.UseVisualStyleBackColor = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.radioButton_left);
+            this.panel1.Controls.Add(this.radioButton_right);
+            this.panel1.Location = new System.Drawing.Point(72, 130);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(234, 73);
+            this.panel1.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(497, 358);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "滑鼠按鍵精靈";
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.RadioButton radioButton_left;
+        private System.Windows.Forms.RadioButton radioButton_right;
+        private System.Windows.Forms.Panel panel1;
+    }
+}
+
---
+++ WindowsFormsApp32/Form1.cs
@@ -0,0 +1,188 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp32
+{
+    public partial class Form1 : Form
+    {
+
+        private BackgroundWorker bw ;
+        private int time1 = 0;
+        private int time2 = 0;
+        Thread tt;
+
+        public Form1()
+        {
+            InitializeComponent();
+            CheckForIllegalCrossThreadCalls = false; //忽略跨執行緒錯誤
+            timer1.Interval = 1000;
+            timer1.Enabled = false;         
+            button2.Enabled = false;
+        }
+
+        private void bw_DoWork(object sender, DoWorkEventArgs e)
+        {
+            MessageBox.Show("OK");
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            time1 = (int)numericUpDown1.Value;
+            numericUpDown1.Enabled = false;
+            button1.Enabled = false;
+            button2.Enabled = true;
+            timer1.Start();
+            button_start();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            numericUpDown1.Enabled = true;
+            button1.Enabled = true;
+            button2.Enabled = false;
+            timer1.Stop();
+            label5.Text = "停止~";
+            label6.Text = "0";
+            tt.Abort();
+
+        }
+
+
+
+        public void button_start()
+        {
+           tt = new Thread(btn_th);
+            tt.Start();
+        }
+
+        private void btn_th()
+        {
+            while (true)
+            {
+                if (radioButton_left.Checked == true)
+                {
+                    DoMouseClick_left_down(0, 0);
+                    DoMouseClick_left_up(0, 0);
+                    label5.Text = "滑鼠左鍵";
+                }else if(radioButton_right.Checked == true)
+                {
+                    //SetCursorPos(0, 0);
+                    DoMouseClick_right_down(0, 0);
+                    DoMouseClick_right_up(0, 0);
+                    //DoMouseClick_up(0, 0);
+                    label5.Text = "滑鼠右鍵";
+                }
+
+                time2 = time1 -1;
+                Thread.Sleep(time1 * 1000);
+
+                //Thread.Sleep(500);
+
+
+            }
+        }
+
+        [DllImport("user32")]
+        static extern bool SetCursorPos(int X, int Y);
+        //private JoyKeys.Core.Joystick joystick;
+        [DllImport("user32.dll", CharSet = CharSet.Auto, CallingConvention = CallingConvention.StdCall)]
+        public static extern void mouse_event(long dwFlags, long dx, long dy, long cButtons, long dwExtraInfo);
+
+        private const int MOUSEEVENTF_MOVE = 0x0001; //移動滑鼠
+        private const int MOUSEEVENTF_MIDDLEDOWN = 0x0020; //模擬滑鼠中鍵按下
+        private const int MOUSEEVENTF_MIDDLEUP = 0x0040; //模擬滑鼠中鍵抬起
+        private const int MOUSEEVENTF_ABSOLUTE = 0x8000; //標示是否採用絕對座標
+
+        private const int MOUSEEVENTF_LEFTDOWN = 0x02; //模擬滑鼠左鍵按下
+        private const int MOUSEEVENTF_LEFTUP = 0x04; //模擬滑鼠左鍵抬起
+        private const int MOUSEEVENTF_RIGHTDOWN = 0x08; //模擬滑鼠右鍵按下
+        private const int MOUSEEVENTF_RIGHTUP = 0x10; //類比滑鼠右鍵抬起
+
+        /// <summary>
+        /// 滑鼠右鍵down
+        /// </summary>
+        public void DoMouseClick_right_down(int X1, int Y1)
+        {
+            //Call the imported function with the cursor's current position
+            int X = Cursor.Position.X;
+            int Y = Cursor.Position.Y;
+            //mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 100, 100);
+            mouse_event(MOUSEEVENTF_RIGHTDOWN, X, Y, X1, Y1);
+        }
+
+
+        /// <summary>
+        /// 滑鼠右鍵up
+        /// </summary>
+        public void DoMouseClick_right_up(int X1, int Y1)
+        {
+            //Call the imported function with the cursor's current position
+            int X = Cursor.Position.X;
+            int Y = Cursor.Position.Y;
+            //mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 100, 100);
+            mouse_event(MOUSEEVENTF_RIGHTUP, X, Y, X1, Y1);
+        }
+
+
+        /// <summary>
+        /// 滑鼠左鍵down
+        /// </summary>
+        public void DoMouseClick_left_down(int X1, int Y1)
+        {
+            //Call the imported function with the cursor's current position
+            int X = Cursor.Position.X;
+            int Y = Cursor.Position.Y;
+            //mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 100, 100);
+            mouse_event(MOUSEEVENTF_LEFTDOWN, X, Y, X1, Y1);
+        }
+
+
+        /// <summary>
+        /// 滑鼠左鍵up
+        /// </summary>
+        public void DoMouseClick_left_up(int X1, int Y1)
+        {
+            //Call the imported function with the cursor's current position
+            int X = Cursor.Position.X;
+            int Y = Cursor.Position.Y;
+            //mouse_event(MOUSEEVENTF_LEFTDOWN | MOUSEEVENTF_LEFTUP, X, Y, 100, 100);
+            mouse_event(MOUSEEVENTF_LEFTUP, X, Y, X1, Y1);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            label6.Text = time2.ToString();
+            time2--;         
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+        //protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        //{
+        //    // 按快捷鍵Ctrl+S執行按鈕的點選事件方法
+        //    if (keyData == (Keys)Shortcut.ShiftF2)
+        //    {
+        //        //button1.PerformClick();
+        //        MessageBox.Show("OK");
+        //        return true;
+        //    }
+        //    return base.ProcessCmdKey(ref msg, keyData); // 其他鍵按預設處理　
+        //}
+
+    }
+}
